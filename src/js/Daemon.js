4d0df6b6a4888aedaa6735ff0a99b3399401d6eb
Mikhail Brann <alunos13@yandex.com>
2024-07-06 22:15:10 +0300

    main blank

--- src/js/Daemon.js
+++
@@ -1,8 +0,0 @@
-import Chapter from './Chapter';
-
-export default class Daemon extends Chapter {
-    constructor(name) {
-        super(name);
-        this._health = 100;
-    }
-}
--- src/js/Magician.js
+++
@@ -1,9 +0,0 @@
-import Chapter from "./Chapter";
-
-export default class Magician extends Chapter {
-    constructor(name) {
-        super(name);
-        this._health = 80;
-        this._manna = 100;
-    }
-}
